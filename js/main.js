b9a9e964778ab2625feb2615784fb13bc2407dc8
solobear <ivansolobear@gmail.com>
2019-12-19 10:47:52 +0200

    dynamic toppings

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const Pizza = {};
+Pizza.toppings = ["olives", "corn", "mushrooms", "onion", 'tomatos']
 
 Pizza.start = () => {
+    Pizza.bindOptions();
+    Pizza.createToppingsList();
+}
+
+Pizza.bindOptions = () => {
     const newBtn = $('#new');
     const saveBtn = $('#save');
     const loadBtn = $('#load');
@@ -9,16 +15,27 @@ Pizza.start = () => {
     loadBtn.click(Pizza.load);
 }
 
+Pizza.createToppingsList = () => {
+    const toppingsMenu = $('#toppings-menu');
+    for (const topping of Pizza.toppings) {
+        const li = $('<li/>');
+        li.attr('class', 'cover-bg');
+        li.attr('id', topping);
+        li.text(topping);
+        toppingsMenu.append(li);
+    }
+}
+
 Pizza.new = () => {
 
 }
 
 Pizza.load = () => {
-    
+
 }
 
 Pizza.save = () => {
-    
+
 }
 
 Pizza.start();
